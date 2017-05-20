4551ed1dcffc9813791f1f6f4a779612d492f994
Ludovic Barman <lbarman@protonmail.ch>
2017-05-20 14:17:47 +0200

    Adds comments

--- kali.py
+++ kali.py
@@ -5,35 +5,23 @@ import os.path
 import sys
 import subprocess
 
-
+# global constants
 REMOTE_URL='git://git.kali.org/packages/{PACKAGE}.git'
 PACKAGE_FOLDER='dist/'
 
+# package definitions
 packages = {}
 packages['info_gathering'] = ['acccheck', 'ace-voip', 'amap', 'automater', 'bing-ip2hosts', 'braa', 'casefile', 'cdpsnarf', 'cisco-torch', 'cookie-cadger', 'copy-router-config', 'dmitry', 'dnmap', 'dnsenum', 'dnsmap', 'dnsrecon', 'dnstracer', 'dnswalk', 'dotdotpwn', 'enum4linux', 'enumiax', 'exploitdb', 'fierce', 'firewalk', 'fragroute', 'fragrouter', 'ghost -phisher', 'golismero', 'goofile', 'hping3', 'intrace', 'ismtp', 'lbd', 'maltego-teeth', 'masscan', 'metagoofil', 'miranda', 'nmap', 'ntop', 'p0f', 'parsero', 'recon-ng', 'set', 'smtp-user-enum', 'snmpcheck', 'sslcaudit', 'sslsplit', 'sslstrip', 'sslyze', 'thc-ipv6', 'theharvester', 'tlssled', 'twofi', 'urlcrazy', 'wireshark', 'wol-e', 'xplico']
 
-
+# post-install scripts (what to do after cloning)
 postInstall = {}
 postInstall["nmap"] = ["./configure", "make", "make install"]
 postInstall["nikto"] = ["echo \"#!/bin/sh\ncd $(pwd)/program; ./nikto.pl\" > nikto.sh", "chmod u+x nikto.sh"]
 
+# how to run the cloned git (the script already guesses if there's an executable, this is for custom stuff)
 runCmds = {}
-runCmds["nikto"] = "$(pwd)/"+PACKAGE_FOLDER+"nikto/nikto.sh"
 
-print ('''
-
-1) Information Gathering            8) Exploitation Tools
-2) Vulnerability Analysis           9) Forensics Tools
-3) Wireless Attacks                 10) Stress Testing
-4) Web Applications                 11) Password Attacks
-5) Sniffing & Spoofing              12) Reverse Engineering
-6) Maintaining Access               13) Hardware Hacking
-7) Reporting Tools                  14) Extra
-                                    
-0) All
-
-''')
-
+# checks if a program is installed by running it in a subprocess and checking for error
 def isInstalled(program):
     try:
         # pipe output to /dev/null for silence
@@ -45,6 +33,7 @@ def isInstalled(program):
     except OSError:
         return False
 
+# if git is not installed, exit 1
 def isGitInstalled():
     if not isInstalled("git"):
         print("git is required for this script to work. Please install it manually, e.g.:")
@@ -55,6 +44,7 @@ def isGitInstalled():
         print("Exiting, status 1.")
         sys.exit(1)
 
+# calls git clone, and wait for exit
 def gitClone(repo, localDir):
     try:
         # pipe output to /dev/null for silence
@@ -67,6 +57,7 @@ def gitClone(repo, localDir):
         print("Exiting, status 1.")
         sys.exit(1)
 
+# main function that checks if we need to clone a package, if so, run the post-install scripts
 def installIfNeeded(package):
     dirName = PACKAGE_FOLDER+package
 
@@ -85,6 +76,7 @@ def installIfNeeded(package):
             for s in postInstall[package]:
                 os.system("cd " + dirName + " && " + s)
 
+# main function that tries to run a program (possibly cloning it before)
 def run(package):
 
     #if package is already installed on the system via package manager, just call it
@@ -106,18 +98,32 @@ def run(package):
             baseName = PACKAGE_FOLDER+package+"/"+package
 
             if os.path.isfile(baseName+".sh"):
-                print("Found an executable:", baseName+".sh")
+                print("Found an executable:", baseName+".sh", "running it... (Ctrl-C to exit)")
                 os.system(baseName+".sh")
             elif os.path.isfile(baseName+".py"):
-                print("Found an executable:", baseName+".py")
+                print("Found an executable:", baseName+".py", "running it... (Ctrl-C to exit)")
                 os.system(baseName+".py")
             elif os.path.isfile(baseName+".pl"):
-                print("Found an executable:", baseName+".pl")
+                print("Found an executable:", baseName+".pl", "running it... (Ctrl-C to exit)")
                 os.system(baseName+".pl")
             #finally, we give up
             else:
                 print("Please run the program in", PACKAGE_FOLDER, package)
 
+def printKaliMenu():
+    print ('''
+1) Information Gathering            8) Exploitation Tools
+2) Vulnerability Analysis           9) Forensics Tools
+3) Wireless Attacks                 10) Stress Testing
+4) Web Applications                 11) Password Attacks
+5) Sniffing & Spoofing              12) Reverse Engineering
+6) Maintaining Access               13) Hardware Hacking
+7) Reporting Tools                  14) Extra
+''')
 
+# check for prerequisites
 isGitInstalled()
+
+printKaliMenu()
+
 run("hashid")
